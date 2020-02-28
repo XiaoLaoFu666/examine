f2c5ba3454c3df28552fdd7d48d32c101cc16938
xiaolaofu666 <1070594049@qq.com>
2020-02-28 15:03:16 +0800

    login Over

--- src/main/java/com/huang/examine/controller/StudentController.java
+++ src/main/java/com/huang/examine/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.huang.examine.controller;
 
 import com.huang.examine.service.StudentService;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+
+    private static Logger log = LoggerFactory.getLogger(StudentController.class);
 
     @Autowired
     private StudentService studentService;
--- src/main/java/com/huang/examine/controller/TeacherController.java
+++ src/main/java/com/huang/examine/controller/TeacherController.java
@@ -1,6 +1,8 @@
 package com.huang.examine.controller;
 
 import com.huang.examine.service.TeacherService;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/teacher")
 public class TeacherController {
+
+
+    private static Logger log = LoggerFactory.getLogger(TeacherController.class);
+
 
     @Autowired
     private TeacherService teacherService;
